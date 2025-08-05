c707c9bdb44ae275789552ea459243163ea7b08b
algaema <algaema@gmail.com>
2025-08-05 14:22:07 +1000

    Update next.config.js

--- site/next.config.js
+++ site/next.config.js
@@ -11,7 +11,7 @@ const nextConfig = {
     contactUrl: 'https://github.com/algaema/kasm-registry/issues',
   },
   reactStrictMode: true,
-  basePath: '/kasm-registry/1.0',
+  basePath: '/kasm-registry/1.1',
   trailingSlash: true,
   images: {
     unoptimized: true,
